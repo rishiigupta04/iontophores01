4df04930d7a650b8a2f1928152c2d5763653ee7d
Rishiraj Gupta <grishi349@gmail.com>
2025-02-24 20:31:27 +0530

    some changes in hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
     <section id="hero" className="section hero">
       <div className="video-background">
         <video autoPlay loop muted playsInline>
-          <source src="/video.mp4" type="video/mp4" />
+          <source src="/vid.mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
       </div>
